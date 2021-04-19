2518127dd987a2d5a3fb246cc665d23e29496283
titas251 <krtitas1@gmail.com>
2021-04-19 21:24:22 +0300

    Last changes to Mybatis files and UI

--- src/main/java/task/lt/usecases/mybatis/TicketsMyBatis.java
+++ src/main/java/task/lt/usecases/mybatis/TicketsMyBatis.java
@@ -6,29 +6,38 @@ import task.lt.mybatis.dao.TicketMapper;
 import task.lt.mybatis.model.Ticket;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.inject.Model;
+import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+import java.io.Serializable;
 import java.util.List;
 
-@Model
-public class TicketsMyBatis {
+@ViewScoped
+@Named
+@Getter @Setter
+public class TicketsMyBatis implements Serializable {
     @Inject
     private TicketMapper ticketMapper;
 
     @Getter @Setter
     private List<Ticket> allTickets;
 
+    @Getter @Setter
+    private Ticket ticketToCreate = new Ticket();
+
     public void loadTrips() {
         this.allTickets = ticketMapper.selectAllWithTrip();
     }
 
-    /*public String getTripNameWithId(Integer tripId){
-        //better way to get all to list and then iterate.
-        return ticketMapper.selectTripNameById(tripId).getName();
-    }*/
-
     @PostConstruct
     public void init() {
         this.loadTrips();
+    }
+
+    @Transactional
+    public String createTicket(){
+        this.ticketMapper.insert(ticketToCreate);
+        return "/mybatis/ticket?faces-redirect=true";
     }
 }
